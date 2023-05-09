3d33cf0edbf510a11d84354cc811d11521d1f0cd
whmehaffey <w.hamish@gmail.com>
2023-05-09 15:26:39 -0700

    Added ROI support, TIFF support, dropped frame checker and Buffer controls.
    Also forces all cameras to buffer 'Oldest First' rather than the default 'Newest First'

--- PySpinMultiCam.py
+++ PySpinMultiCam.py
@@ -31,6 +31,8 @@ class MultiCamObj:
     width=[];
     height=[];
     framerate=[];
+    exposure=[];
+    exposuretime=[];
     serials='';
     
     def __init__(self, *serials):
@@ -55,22 +57,41 @@ class MultiCamObj:
             # Set Cam #1 with driver setup on Hirose 
             self.cam[0].LineSelector.SetValue(pyspin.LineSelector_Line2)
             self.cam[0].V3_3Enable.SetValue(True)
+            self.cam[0].AcquisitionFrameRateEnable.SetValue(False)
+            self.cam[0].ExposureAuto.SetValue(False)            
+            s_node_map = self.cam[0].GetTLStreamNodeMap()
+            #Up buffers and set to OldestFirst (defaults to newest, fucking assholes)
+            
+            buffer_count = pyspin.CIntegerPtr(s_node_map.GetNode('StreamBufferCountManual'))
+            #pdb.set_trace();
+            buffer_count.SetValue(buffer_count.GetMax());
+            print(buffer_count.GetMax());
+            handling_mode = pyspin.CEnumerationPtr(s_node_map.GetNode('StreamBufferHandlingMode'))
+            handling_mode_entry = handling_mode.GetEntryByName('OldestFirst')
+            handling_mode.SetIntValue(handling_mode_entry.GetValue())            
             self.pixelformat=self.cam[0].PixelFormat();
-            self.height.append(self.cam[0].SensorHeight());
-            self.width.append(self.cam[0].SensorWidth());        
-            self.framerate=self.cam[0].AcquisitionFrameRate();
+            self.height.append(self.cam[0].Height());
+            self.width.append(self.cam[0].Width());        
+            self.framerate=self.cam[0].AcquisitionFrameRate();           
             self.exposuretime=self.cam[0].ExposureTime();
             
             if (self.camcount > 1):
-                for i in range(1,self.camcount):   #Syncing setup for follower BlackFlys.             
+                for i in range(1,self.camcount):   #Syncing setup for follower BlackFlys.                    
                     self.cam[i].TriggerMode.SetValue(pyspin.TriggerMode_Off)
                     self.cam[i].TriggerSource.SetValue(pyspin.TriggerSource_Line3)
                     self.cam[i].TriggerOverlap.SetValue(pyspin.TriggerOverlap_ReadOut)
                     self.cam[i].TriggerMode.SetValue(pyspin.TriggerMode_On)
-                    self.height[i].append(self.cam[i].SensorHeight());
-                    self.width[i].append(self.cam[i].SensorWidth());                    
+                    self.height.append(self.cam[i].Height()); #SensorHeight for unbinned!
+                    self.width.append(self.cam[i].Width());                    
                     self.cam[i].AcquisitionFrameRateEnable.SetValue(False)
                     self.cam[i].ExposureAuto.SetValue(False)
+                    s_node_map = self.cam[i].GetTLStreamNodeMap()
+                    buffer_count = pyspin.CIntegerPtr(s_node_map.GetNode('StreamBufferCountManual'))
+                    buffer_count.SetValue(200);
+                    handling_mode = pyspin.CEnumerationPtr(s_node_map.GetNode('StreamBufferHandlingMode'))
+                    handling_mode_entry = handling_mode.GetEntryByName('OldestFirst')
+                    handling_mode.SetIntValue(handling_mode_entry.GetValue())
+                    
            
         except ValueError as err:
             print(err.args)
@@ -94,8 +115,10 @@ class MultiCamObj:
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex)
             return False;
+        
+    #def GetExposureMode(self,mode):
 
-    def SetExposureMode(self,mode='once'):
+    def SetExposureMode(self,mode):
      """sets the exposure mode of the camera.  Mode should be once, continuous, or off."""
      if mode=='once':
       try:
@@ -134,7 +157,6 @@ class MultiCamObj:
             print('Error: %s' % ex)
             return False;
 
-
     def Stop(self):
         try:
             for i in range(self.camcount): #Stop beginning at the primary;
@@ -149,26 +171,53 @@ class MultiCamObj:
         FramesRemaining=self.cam[0].TransferQueueCurrentBlockCount()        
         return FramesRemaining;
 
-    def GetNextImage(self):
-        images=[]
+    def GetNextImage(self):         
+        Images=[];
         try:                     
-                for i in range(self.camcount):                   
+                for i in range(self.camcount):                  
                    image=self.cam[i].GetNextImage(1000);
-                   images.append(image.GetNDArray())
+                   im = image.GetNDArray()
+                   Images.append(im)
                    image.Release()
                    
         except pyspin.SpinnakerException as ex:
               print('Error: %s' % ex)
               return False;
-        return images;
+        return Images;
 
-    def GetAllBufferedImages(self):        
+    def SnapImage(self):         
+        Images=[];
+        self.SetExposureMode('once');
+        t=self.cam[0].AcquisitionMode.GetCurrentEntry()
+        OldValues=t.GetDisplayName()
+        self.Start();       
+        
+        try:                     
+                for i in range(self.camcount):                   
+                   image=self.cam[i].GetNextImage(1000);
+                   im = image.GetNDArray()
+                   Images.append(im)
+                   image.Release()
+                   
+        except pyspin.SpinnakerException as ex:
+              print('Error: %s' % ex)
+              return False;
+        self.Stop();
+ 
+        self.SetExposureMode('continuous');
+   
+        return Images;    
+
+    def GetAllBufferedImages(self):
+                
         images=[]
         try:
             for j in range(self.FramesInBuffer()):
-                for i in range(self.camcount):            
+                for i in range(self.camcount):
+
                    image=self.cam[i].GetNextImage(1000);
-                   images.append(image.GetNDArray())
+                   im = image.GetNDArray()
+                   images.append(im)
                    image.Release()
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex)
@@ -178,7 +227,7 @@ class MultiCamObj:
     def SetExposureTime(self,exposure): # in uS
         try:            
             # Set 1st Cam, others follow.
-            amc.cam[0].ExposureAuto.SetValue(pyspin.ExposureAuto_Off)
+            self.cam[0].ExposureAuto.SetValue(pyspin.ExposureAuto_Off)
             exposure = min(self.cam[0].ExposureTime.GetMax(), exposure)
             self.cam[0].ExposureTime.SetValue(exposure);
             self.exposuretime=self.cam[0].ExposureTime();
@@ -202,13 +251,16 @@ class MultiCamObj:
         return False          
 
     def AcquireImageSequence(self, framecount):
+        from PIL import Image
+        
         try:            
             timeout=500; # Something has gone wrong at this point.
             
             for i in range(framecount):
                   for j in range(self.camcount):    
                       image=self.cam[j].GetNextImage(timeout)
-                      images.append(image.GetNDArray())
+                      im = (image.GetNDArray())
+                      images.append(im)
                       image.Release()
             return images    
         except pyspin.SpinnakerException as ex:
@@ -217,10 +269,13 @@ class MultiCamObj:
 
     def SetBinning(self, binningradius):
         try:                                  
-                  for j in range(self.camcount):    
+           for j in range(self.camcount):    
                       self.cam[j].BinningVertical.SetValue(binningradius);
-                      self.cam[j].BinningHorizontal.SetValue(binningradius);
-             
+                      self.cam[j].BinningHorizontal.SetValue(binningradius);                      
+                      self.height[j]=(self.cam[j].Height());
+                      self.width[j]=(self.cam[j].Width());        
+      
+                        
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex);
         return False           
@@ -231,28 +286,33 @@ class MultiCamObj:
             avi_handler=[];
             avi_header_settings=[];
            # tme=int(time.time())
-            for j in range(self.camcount): 
+            for j in range(self.camcount):
+                
                 avi_handler.append(pyspin.SpinVideo());
-                avi_header_settings[j]=pyspin.AVIOption();
+                avi_header_settings.append(pyspin.AVIOption());
                 avi_header_settings[j].frameRate=self.framerate;
-                avi_header_settings[j].height=self.cam[j].Height();
-                avi_header_settings[j].width=self.cam[j].Width();                
+                avi_header_settings[j].height=self.height[j];
+                avi_header_settings[j].width=self.width[j];                 
                 filename=filename+'CAM_'+str(j)+'.avi'   
                 avi_handler[j].Open(filename,avi_header_settings[j])
-                
-            for i in range(framecount):
-                  for j in range(self.camcount):    
+            i=0;    
+            #while (0< framecount): #for i in range(framecount):
+                           
+               #if (self.FramesInBuffer()>1):                       
+            for j in range(self.camcount):    
                       image=self.cam[j].GetNextImage()                      
                       avi_handler[j].Append(image)
-                      image.Release()                                        
+                      image.Release()
+                      i=i+1;
                         
             for j in range(self.camcount):                       
                 avi_handler[j].Close()
-        
+                
+            FailedCount=self.CheckLostFrames();
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex);
-        return False         
-
+        return FailedCount
+    
     def SaveImageTimeSeriesAVI(self, time,filename): #in S
         from math import ceil
         framecount=ceil(self.framerate*time);
@@ -262,28 +322,32 @@ class MultiCamObj:
             avi_handler=[];
             avi_header_settings=[];
           #  tme=int(time.time())
-                                      
+                                  
             for j in range(self.camcount): 
                 avi_handler.append(pyspin.SpinVideo());
-                avi_header_settings[j]=pyspin.AVIOption();
+                avi_header_settings.append(pyspin.AVIOption());
                 avi_header_settings[j].frameRate=self.framerate;
-                avi_header_settings[j].height=self.cam[j].Height();
-                avi_header_settings[j].width=self.cam[j].Width();                
+                avi_header_settings[j].height=self.height[j];
+                avi_header_settings[j].width=self.width[j];                
                 filename=filename+'CAM_'+str(j)+'.avi'   
                 avi_handler[j].Open(filename,avi_header_settings[j])
-                
+
             for i in range(framecount):
                   for j in range(self.camcount):    
                       image=self.cam[j].GetNextImage()                      
                       avi_handler[j].Append(image)
-                      image.Release()                            
+                      image.Release()
+                      
+                      
             for j in range(self.camcount):                       
                 avi_handler[j].Close()
         
+            FailedCount=self.CheckLostFrames();
+            print(FailedCount)                      
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex);
-        return False    
-
+        return FailedCount
+    
     def SaveImageSequenceTIFF(self, framecount,filename): 
         from libtiff import TIFF
         from PIL import Image
@@ -302,17 +366,17 @@ class MultiCamObj:
                   for j in range(self.camcount):
                       
                       image=self.cam[j].GetNextImage()
-                      #images=image.GetNDArray()
-                      im = Image.fromarray(image.GetNDArray())
+                      
+                      im = Image.fromarray(image.GetNDArray())                      
                 #      im = im.resize((int(self.cam[j].Width()),int(self.cam[j].Height())), Image.Resampling.LANCZOS)                        
                       tif_handler[j].write_image(im, compression = None)                      
                         
             for j in range(self.camcount):                       
                 tif_handler[j].close()
-        
+            FailedCount=self.CheckLostFrames()
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex);
-        return False
+        return FailedCount
 
     def SaveImageTimeSeriesTIFF(self, framecount,filename): 
         from libtiff import TIFF
@@ -324,7 +388,7 @@ class MultiCamObj:
             timeout=500; # Something has gone wrong at this point.
             tif_handler=[];
             tif_header=[];
-           # tme=int(time.time())
+            
             for j in range(self.camcount):
                 
                 filename=filename+'CAM_'+str(j)+'.tiff'   
@@ -333,33 +397,105 @@ class MultiCamObj:
             for i in range(framecount):
                   for j in range(self.camcount):
                       
-                      image=self.cam[j].GetNextImage()
-                      #images=image.GetNDArray()
-                      im = Image.fromarray(image.GetNDArray())
-                #      im = im.resize((int(self.cam[j].Width()),int(self.cam[j].Height())), Image.Resampling.LANCZOS)                        
+                      image=self.cam[j].GetNextImage()                      
+                      im = Image.fromarray(image.GetNDArray())                
                       tif_handler[j].write_image(im, compression = None)                      
                         
             for j in range(self.camcount):                       
                 tif_handler[j].close()
         
+            FailedCount=self.CheckLostFrames();
         except pyspin.SpinnakerException as ex:
             print('Error: %s' % ex);
-        return False    
+        return FailedCount 
 
+    def CheckLostFrames(self): # X,Y Offsets, Width, Height
+        
+            MissingFramesCount=[0]*self.camcount;
+            try:
+                for j in range(self.camcount):
+                    s_node_map = self.cam[j].GetTLStreamNodeMap()
+                    LostFrames = pyspin.CIntegerPtr(s_node_map.GetNode('StreamLostFrameCount'))
+                    MissingFramesCount[j]=LostFrames.GetValue();
+            except pyspin.SpinnakerException as ex:
+                print('Error: %s' % ex);
+            return MissingFramesCount;   
+    #### Testing stuff
+
+    def SetBufferSize(self, bufsize): # X,Y Offsets, Width, Height
+            
+
+            try:
+                for j in range(self.camcount):
+                    s_node_map = self.cam[j].GetTLStreamNodeMap()
+                    buffer_count = pyspin.CIntegerPtr(s_node_map.GetNode('StreamBufferCountManual'))
+                    buffer_count.SetValue(bufsize);                
+            except pyspin.SpinnakerException as ex:
+                print('Error: %s' % ex);
+            return False ;
+        
+    def ClearROI(self): # X,Y Offsets, Width, Height        
+            
+            try:
+                for j in range(self.camcount):
+                    self.cam[j].AasRoiEnable.SetValue(False);
+                    self.height[j]=self.cam[j].Height();
+                    self.width[j]=self.cam[j].Width();                    
+            except pyspin.SpinnakerException as ex:
+                print('Error: %s' % ex);
+            return False
+        
+    def SetROI(self, rois): # X,Y Offsets, Width, Height
+                            
+            try:
+
+                for j in range(self.camcount):                    
+                    self.cam[j].AasRoiEnable.SetValue(True);                    
+                    self.cam[j].AasRoiWidth.SetValue(rois[j][2])
+                    self.cam[j].AasRoiHeight.SetValue(rois[j][3])                    
+                    self.cam[j].AasRoiOffsetX.SetValue(rois[j][0])
+                    self.cam[j].AasRoiOffsetY.SetValue(rois[j][1])
+                    self.height[j]=rois[j][2]; #self.cam[j].Height();
+                    self.width[j]=rois[j][3]; #self.cam[j].Width();
+                    print('Camera ' + str(j) + ' -> ' +str(self.height[j]) + ' x ' + str(self.width[j]))
+                          
+                    
+            except pyspin.SpinnakerException as ex:
+                print('Error: %s' % ex);
+            return False    
 #### Testing stuff
-
-cams=listCams();       
-amc=MultiCamObj(cams[0]); #The 2 color blackfly
-
+##import cv2
+##cams=listCams();       
+##amc=MultiCamObj(cams[0]); #The 2 color blackfly
+##rois=[]
+##j=0
+##Images=amc.SnapImage();
+##roi=cv2.selectROI('Choose ROI - Enter to select, "c" to cancel',Images[0])
+##rois.append(roi);
+##cv2.destroyAllWindows()
+##amc.cam[j].AasRoiEnable.SetValue(True);                    
+##amc.cam[j].AasRoiWidth.SetValue(rois[j][2])
+##amc.cam[j].AasRoiHeight.SetValue(rois[j][3])                    
+##amc.cam[j].AasRoiOffsetX.SetValue(rois[j][0])
+##amc.cam[j].AasRoiOffsetY.SetValue(rois[j][1])
+##amc.height[j]=amc.cam[j].Height();
+##amc.width[j]=amc.cam[j].Width();
 #amc.Start();
+##s_node_map = amc.cam[0].GetTLStreamNodeMap()
+##buffer_count = pyspin.CIntegerPtr(s_node_map.GetNode('StreamBufferCountManual'))
+##handling_mode = pyspin.CEnumerationPtr(s_node_map.GetNode('StreamBufferHandlingMode'))
+##handling_mode_entry = handling_mode.GetEntryByName('OldestFirst')
+##handling_mode.SetIntValue(handling_mode_entry.GetValue())
 
 
 #time.sleep(0.05);
 #amc.SetBinning(2);
-amc.Start();
+#amc.Start();
+#Img=amc.GetNextImage();
+#amc.Stop();
 #amc.SaveImageTimeSeries(2,'filename')
-amc.SaveImageSequenceTIFF(100,'filename');
-amc.Stop();    
+#amc.SaveImageSequenceTIFF(100,'filename');
+#amc.Stop();    
 #frames=amc.FramesInBuffer();
 #print(frames);
 
